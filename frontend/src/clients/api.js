35c559ae55246133742efa63360e10de4048e97b
Mithra <mithra29.venu@gmail.com>
2026-03-27 11:57:53 -0400

    Added env

--- frontend/src/clients/api.js
+++ frontend/src/clients/api.js
@@ -1,20 +1,28 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const token = () => localStorage.getItem('token')
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const token = () => localStorage.getItem('token');
 
 export const userClient = axios.create({
-     baseURL: 'http://localhost:3000/api/users',
-     headers: {
-          Authorization: `Bearer ${token()}`
-     }
-})
+  baseURL: BASE_URL + "/api/users",
+  headers: {
+    Authorization: `Bearer ${token()}`
+  }
+});
 
 export const postClient = axios.create({
-     baseURL: 'http://localhost:3000/api/posts'
-})
+  baseURL: `${BASE_URL}/api/posts`
+  // baseURL: "http://localhost:3000/api/posts",
+  // headers: {
+  //   Authorization: `Bearer ${token}`
+  // }
+});
 
-// use the latest version of the token in local storage
-postClient.interceptors.request.use((req) => {
-     if (token()) req.headers.Authorization = `Bearer ${token()}`
-     return req
+postClient.interceptors.request.use((req) =>{
+// const token = localStorage.getItem('token');
+  if(token()){
+    req.headers.Authorization = `Bearer ${token()}`
+  }
+  return req;
 })
